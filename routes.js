67021e749d6406b5a1e1f030cb2b863672620bca
tylerjusfly <tylerjusfly1@gmail.com>
2022-07-06 21:13:49 +0100

    adding json file for future use

--- routes.js
+++ routes.js
@@ -1,38 +1,56 @@
-const express = require('express')
-const { getWastes } = require('./waste.api')
-const router = express.Router()
-const _ = require('lodash')
+const express = require("express");
+const { getWastes, Looping } = require("./waste.api");
+const router = express.Router();
+const _ = require("lodash");
+const axios = require("axios");
 
-router.get('/', async(req, res) => {
-  let searchQuery = req.query.query
+router.get("/", async (req, res) => {
+  let searchQuery = req.body.search;
   let wastes = await getWastes();
 
   try {
-    // Checking if query is not empty 
-    if( !_.isEmpty(searchQuery)) {
+    // Checking if query is not empty
+    if (!_.isEmpty(searchQuery)) {
       // Trim spaces and convert to lower case
       searchQuery = searchQuery.trim().toLowerCase();
       // filter
-      let wastes = wastes.filter(
-  
+      let was = wastes.filter(
         /* Trim and covert waste.title || keywords || category to lowercase. 
         then check if it includes the searchQuery */
-        waste => waste.title.trim().toLowerCase().includes(searchQuery) ||
-        waste.keywords.trim().toLowerCase().includes(searchQuery) ||
-        waste.category.trim().toLowerCase().includes(searchQuery)
-      )
-  
+        (waste) =>
+          waste.title.trim().toLowerCase().includes(searchQuery) ||
+          waste.keywords.trim().toLowerCase().includes(searchQuery) ||
+          waste.category.trim().toLowerCase().includes(searchQuery)
+      );
     }
-    res.send(wastes)
-    
-
+    res.send(wastes);
   } catch (error) {
-    console.log(error)
-    res.send('server error')
+    console.log(error);
+    res.send("server error");
   }
-
-
-
 });
 
-module.exports = router
+router.get("/a", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://genius.p.rapidapi.com/search",
+    params: { q: "Tupac shakur" },
+    headers: {
+      "X-RapidAPI-Key": "1e21171e7cmsh84a2ae759069572p150da6jsn797f6688d8dc",
+      "X-RapidAPI-Host": "genius.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      res.json(response.data.response);
+    })
+    .catch(function (error) {
+      res.json(error);
+    });
+});
+
+//router.get("/load", Looping);
+
+module.exports = router;
--- waste.api.js
+++ waste.api.js
@@ -1,17 +1,36 @@
-const axios = require('axios');
+const axios = require("axios");
+//const file = require("./loop.json");
 
-exports.getWastes = async(req, res) => {
-  const url = 'https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=100';
+exports.getWastes = async (req, res) => {
+  const url = "https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=100";
 
   try {
     // get data from api
     const data = await axios.get(url);
-  
-    return data.data;
-    
-  } catch (error) {
-    console.log(error)
-    res.json('server error')
-  }
 
-}
+    return data.data;
+  } catch (error) {
+    console.log(error);
+    res.json("server error");
+  }
+};
+
+// exports.Looping = (req, res) => {
+//   try {
+//     res.json(
+//       file.hits.map((e) => {
+//         return {
+//           artist_names: e.result.artist_names,
+//           image_url: e.result.song_art_image_url,
+//           title: e.result.title,
+//           release_date: e.result.release_date_for_display,
+//           verified: e.result.primary_artist.is_verified,
+//           lyrics: e.result.url,
+//         };
+//       })
+//     );
+//   } catch (error) {
+//     console.log(error);
+//     res.json("server error");
+//   }
+// };
